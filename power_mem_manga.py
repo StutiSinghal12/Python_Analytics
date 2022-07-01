8e4e66c9a80bded01c1ff6dc0b86eef320aefccc
Stuti Singhal <78038262+StutiSinghal12@users.noreply.github.com>
2022-07-01 11:55:06 +0530

    Create power_mem_manga.py

---
+++ power_mem_manga.py
@@ -0,0 +1,4 @@
+question---> print a^b+c^d , where a,b,c,d are interger (fyi there is not limit to size of integer in python)
+
+a,b,c,d = (int(input()),int(input()),int(input()),int(input()));
+print(pow(a,b) + pow(c,d));
